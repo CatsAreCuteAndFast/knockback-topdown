dd2b97c61c3dc7fe5b58e5be0006d4660215af69
Mark <mark.peter.grossman@gmail.com>
2024-08-20 12:24:40 +0200

    Added state machine and enemy

---
+++ scripts/states/wander.gd
@@ -0,0 +1,25 @@
+extends State
+class_name Wander
+
+@export var enemy : CharacterBody2D
+@export var moveSpeed := 10
+
+var move_direction : Vector2
+var wander_time : float
+
+func randomize_wander():
+	move_direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
+	wander_time = randf_range(1, 3)
+	
+func Enter():
+	randomize_wander()
+	
+func Update(delta : float):
+	if wander_time > 0:
+		wander_time -= delta
+	else:
+		randomize_wander()
+		
+func PhysicsUpdate(delta : float):
+	if enemy:
+		enemy.velocity = move_direction * moveSpeed
